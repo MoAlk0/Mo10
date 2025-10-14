8da6d3fec10df8b76273cc97a1750d985d667158
MoAlk0 <samirmohammad503@gmail.com>
2025-10-14 23:03:31 +0100

    Delete Web directory

--- Web/js/script.js
+++
@@ -1,6 +0,0 @@
-function changeBackground() {
-const colors = ["#334143ff", "#d4cf9dff", "#c39496ff", "#be9dd1ff", "#1c610eff"];
-const random = Math.floor(Math.random() * colors.length);
-document.body.style.backgroundColor = colors[random];
-document.querySelector("button").style.color = colors[random];
-}
